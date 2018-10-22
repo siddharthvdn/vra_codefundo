daf4da0ba690abd0ed5e8279f9ca655ce617cc79
kaushiksk <kaushikskalmady@gmail.com>
2018-10-22 19:37:43 +0530

    Reordered dir

--- sahaay/__init__.py
+++ sahaay/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask import render_template
 
 def create_app(test_config=None):
-    app = Flask(__name__,  instance_relative_config=True, template_folder='../templates', static_folder='../static')
+    app = Flask(__name__,  instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
     )
