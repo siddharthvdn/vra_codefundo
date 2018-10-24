f13ab76d8ca5169d894524850568bc0774bf02b0
kaushiksk <kaushikskalmady@gmail.com>
2018-10-24 12:25:21 +0530

    Modifies login functionality, adds init_db

--- sahaay/auth.py
+++ sahaay/auth.py
@@ -2,7 +2,7 @@
 # @Author: Kaushik S Kalmady
 # @Date:   2018-10-22 21:49:59
 # @Last Modified by:   kaushiksk
-# @Last Modified time: 2018-10-23 12:43:36
+# @Last Modified time: 2018-10-24 11:38:04
 import functools
 from flask import (
     Blueprint, flash, redirect, g, render_template, request, session, url_for
@@ -17,8 +17,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = session["username"]
         email = request.form['email']
         longitude = request.form['longitude']
         latitude = request.form['latitude']
@@ -27,22 +26,27 @@ def register():
 
         if not username:
             error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
         elif db.users.find_one({"username": username}) is not None:
             error = 'User {} is already registered.'.format(username)
 
         if error is None:
             user_data = {'username': username,
-                         'password': generate_password_hash(password),
                          'email': email,
-                         'longitude': longitude,
-                         'latitude': latitude}
+                         'loc': [longitude, latitude]}
 
             db.users.insert(user_data)
-            return redirect(url_for('auth.login'))
 
-        flash(error)
+            db.login.update_one(
+                    {"username": username},
+                    {
+                        '$set':{
+                            'verified': True
+                        }
+                    })
+            flash("Details Updated Successfully!", "success")
+            return redirect(url_for('interact.index'))
+
+        flash(error, "error")
 
     return render_template('auth/register.html')
 
@@ -53,7 +57,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        user = db.users.find_one({"username": username})
+        user = db.login.find_one({"username": username})
         error = None
 
         if user is None:
@@ -64,10 +68,15 @@ def login():
         if error is None:
             session.clear()
             session['username'] = user['username']
-            print user
+            flash("Welcome {}!".format(user['username']), "success")
+
+            print 
+            if not user["verified"]:
+                return render_template('auth/register.html') 
+                
             return redirect(url_for('interact.index'))
 
-        flash(error)
+        flash(error, 'error')
 
     return render_template('auth/login.html')
 
@@ -81,11 +90,12 @@ def logout():
 @bp.before_app_request
 def load_logged_in_user():
     username = session.get('username')
-
+    from . import config
     if username is None:
         g.user = None
     else:
         g.user = db.users.find_one({"username": username})
+    g.api_key = config.get_maps_api_key()
 
 
 def login_required(view):
