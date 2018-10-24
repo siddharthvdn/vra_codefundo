b3f0f48c8a5a6f82536a373993381921df6aa7c9
siddharthvdn <siddharthvdn@gmail.com>
2018-10-24 13:15:53 +0530

    Cleans up merge conflict files

--- sahaay/config_LOCAL_11586.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Kaushik S Kalmady
-# @Date:   2018-10-22 21:36:24
-# @Last Modified by:   kaushiksk
-# @Last Modified time: 2018-10-22 21:57:33
-from pymongo import MongoClient
-
-mongo = MongoClient('mongodb://localhost:27017/')
