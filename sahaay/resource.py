f8b77b98c8f00ef77ab8a309f65ecb8d364eefb4
siddharthvdn <siddharthvdn@users.noreply.github.com>
2018-10-25 14:41:29 +0530

    Accept request (#6)
    * Add accept feature, change supply to distributed
    
    * Add reject feature
    
    * Verify functions
    
    * Add order-summary feature and corresponding html page
    
    * Add order-summary display conditions
    
    * Add minor styling to time displays

--- sahaay/resource.py
+++ sahaay/resource.py
@@ -1,11 +1,11 @@
 from flask import (
-    Blueprint, flash, redirect, g, render_template, request, session, url_for
+    Blueprint, flash, jsonify, redirect, g, render_template, request, session, url_for
 )
 from config import mongo
 from pymongo import GEO2D
 
-from bson.son import SON
-
+from bson.son import SON 
+from bson import ObjectId
 import datetime
 
 db = mongo["sahaay"]
@@ -13,7 +13,7 @@ db = mongo["sahaay"]
 bp = Blueprint('resource', __name__, url_prefix='/resource')
 
 def repeat():
-    loosers = db.requests.find({'status':0})
+    loosers = db.requests.find({'qty':{'$gt': 0}})
 
     now = datetime.datetime.now()
 
@@ -21,23 +21,26 @@ def repeat():
         elapsed = divmod((now - looser['last_time']).total_seconds(), 60)[0]
 
         if elapsed > 60 or len(looser['to']) == 0:
-            query = {"location": SON([("$near", looser['location']), ("$minDistance", looser['radius']), ("$maxDistance", looser['radius']+5000)])}
+            current = db.users.find_one({'username': looser['from']})
+
+            query = {"location": SON([("$near", current['location']), ("$minDistance", looser['radius']), ("$maxDistance", looser['radius']+5000)])}
             neighbours = list(db.users.find(query))
             print neighbours
             
             camps = looser['to']
-            for neighbour in neighbours:   
-                print neighbour['username']         
-                current = db.inventory.find_one({'username': neighbour['username'], 'idx': looser['idx']})
-                
-                if current is not None and current['qty'] > looser['qty']:
-                    #send request to this camp
-                    camps.append(current['user'])
-                    print current['username']   
+            for neighbour in neighbours: 
+                if not neighbour['username'] == looser['from']:  
+                    print neighbour['username']         
+                    current = db.inventory.find_one({'username': neighbour['username'], 'idx': looser['idx']})
+                    
+                    if current is not None and current['qty'] > looser['qty']:
+                        #send request to this camp
+                        camps.append(current['user'])
+                        print current['username']   
 
-            db.requests.update_one(
+            db.requests.update(
                 {
-                    '_id':looser['_id']
+                    '_id':ObjectId(looser['_id'])
                 },
                 {'$set': 
                     {
@@ -62,9 +65,7 @@ def request_resource():
         
         item = db.users.find_one({'username': g.user['username']})
 
-        item['location']
-
-        db.users.create_index([('location', GEO2D)])
+        # db.users.create_index([('location', GEO2D)])
         #neighbours = list(db.users.find({'location': {"$near": item['location']}}).limit(3))
         query = {"location": SON([("$near", item['location']), ("$minDistance", 0), ("$maxDistance", 5000)])}
         neighbours = list(db.users.find())
@@ -72,20 +73,21 @@ def request_resource():
         
         camps = []
         for neighbour in neighbours:   
-            print neighbour['username']         
-            current = db.inventory.find_one({'username': neighbour['username'], 'idx': idx})
-            
-            if current is not None and current['qty'] > qty:
-                #send request to this camp
-                camps.append(current['username'])
-                print current['username']
+            if not neighbour['username'] == g.user['username']:
+                print neighbour['username']         
+                current = db.inventory.find_one({'username': neighbour['username'], 'idx': idx})
+                
+                if current is not None and current['qty'] > qty:
+                    #send request to this camp
+                    camps.append(current['username'])
+                    print current['username']
+
 
         request_post = {'from':g.user['username'], 
                         'idx': idx,
                         'qty': qty,
                         'to':camps, 
-                        'status':0, 
-                        'donor':None, 
+                        'logs':None, 
                         'radius':5000, 
                         'ini_time':datetime.datetime.now(),
                         'last_time': datetime.datetime.now()
@@ -128,6 +130,85 @@ def update_resource():
 
         flash("Inventory updated successfuly!", "success")
         
+
     return render_template('resource/update.html')
 
+@bp.route('/accept', methods=['POST'])
+def accept_request():
+    if request.method == 'POST':
+        _id = request.form['_id']
+        sup = request.form['supply']
 
+        item = db.requests.find_one({'_id': ObjectId(_id)})
+
+        sup = min(sup, item['qty'])
+
+        log = g.user['username'] + ' supplied ' + str(sup) + ' units of ' + item['idx']
+
+        db.requests.update(
+            {'_id': ObjectId(_id)},
+            {'$set': {'qty': item['qty'] - sup}}, 
+            upsert=False)
+
+        db.requests.update(
+            {'_id': ObjectId(_id)},
+            {'$push' : {'logs': log} },
+            upsert=False)
+
+        db.inventory.update(
+            {
+                'username': g.user['username'],
+                'idx': item['idx']            
+            },
+            {'$inc': {'qty': -sup}},  
+            upsert=False)
+
+        return jsonify({'supplied': sup})
+
+
+@bp.route('/reject', methods=['POST'])
+def reject_request():
+    if request.method == 'POST':
+
+        _id = request.form['_id']
+
+        item = db.requests.find_one({'_id': ObjectId(_id)})
+
+        log = g.user['username'] + ' rejected request for ' + str(int(item['qty'])) + ' units of ' + str(item['idx'])
+
+        db.requests.update(
+            {'_id': ObjectId(_id)},
+            {'$push' : {'logs': log} }, 
+            upsert=False)       
+
+        return jsonify({'supplied': 0})
+
+@bp.route('/terminate', methods=['POST'])
+def terminate_request():
+    if request.method == 'POST':
+
+        _id = request.form['_id']
+
+        item = db.requests.find_one({'_id': ObjectId(_id)})
+
+        log = g.user['username'] + ' terminated the request' 
+
+        db.requests.update(
+            {'_id': ObjectId(_id)},
+            {'$push' : {'logs': log} }, 
+            upsert=False)  
+
+        db.requests.update(
+            {'_id': ObjectId(_id)},
+            {'$set': {'qty': 0}}, 
+            upsert=False)  
+
+        return jsonify({'supplied': 1})
+
+@bp.route('/order-summary/<order_id>', methods=['GET'])
+def order_summary(order_id):
+    item = db.requests.find_one({'_id': ObjectId(order_id)})
+
+    item['_id'] = str(item['_id'])
+
+    return render_template('resource/order-summary.html', item=item)
