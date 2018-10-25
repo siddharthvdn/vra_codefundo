ae21e0c7253a286b034579c13a637eff2a918ec7
siddharthvdn <siddharthvdn@gmail.com>
2018-10-26 00:05:01 +0530

    Fix import error in sahaay/auth.py

--- sahaay/auth.py
+++ sahaay/auth.py
@@ -8,7 +8,7 @@ from flask import (
     Blueprint, flash, redirect, g, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-from config import mongo
+from .config import mongo
 
 db = mongo["sahaay"]
 
